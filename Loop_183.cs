8ca7b3f3a8b0c048107316f6ab3703c9cf053af0
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-25 23:35:44 -0700

    added files

---
+++ Loop_183.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        // Continue in while-loop until index is equal to 10.
+        int i = 0;
+        while (i < 10)
+        {
+            Console.Write("While statement ");
+            // Write the index to the screen.
+            Console.WriteLine(i);
+            // Increment the variable.
+            i++;
+        }
+    }
+}
+
---
+++ Loop_183b/Loop_183b/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+
+public class Program
+{
+	public static void Main()
+	{
+		int i = 0;
+
+		do
+		{
+			Console.WriteLine("i = {0}", i);
+			i++;
+
+		} while (i < 5);
+	}
+}
