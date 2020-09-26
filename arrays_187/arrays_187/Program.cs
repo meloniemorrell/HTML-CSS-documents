d984ddbb14f68c941154a6bdd41086776bc14d06
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-26 10:39:47 -0700

    added files

---
+++ arrays_187/arrays_187/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+        static void Main()
+        {
+        int[] numArray = new int[5];
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 200;
+        numArray[4] = 5000;
+
+        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+
+        Console.WriteLine(numArray[3]);
+        Console.ReadLine();
+    }
+
+}
