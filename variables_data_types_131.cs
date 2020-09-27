d39c839f5cb8dd666119a100116e7d0abfcaaabf
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-27 12:08:36 -0700

    added files

---
+++ variables_data_types_131.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is" + yourName);
+            //Console.ReadLine();
+
+            \
+
+            
+        }
+    }
+}
